24d3b694768d3966ad6c761e8ce30f5d38237b8d
alexcodeable <alexokomado@gmail.com>
2022-03-20 18:47:46 +0100

    fixed search error of not searching article content
    Signed-off-by: alexcodeable <alexokomado@gmail.com>

--- app/models/article.rb
+++ app/models/article.rb
@@ -17,6 +17,8 @@ class Article < ApplicationRecord
 
   validates :title, presence: true, length: {minimum: 6}
   has_rich_text :content
+  has_one :content, class_name: 'ActionText::RichText', as: :record, dependent: :destroy
+
   has_one_attached :image
   
   
